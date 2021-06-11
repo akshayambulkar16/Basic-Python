8b679d324c31f3ec7e2288700b99d12bf3f45b29
Akshay Ambulkar <82163456+akshayambulkar16@users.noreply.github.com>
2021-06-11 15:47:53 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,10 @@
+# simple program (this is single line coment)
+# For Multiline Comment
+"""this is how we do multiple line comment
+    for this we use colens"""
+# statements merge
+"""to merge the to print statements use end operation (end="")"""
+print("hello pycharm",end=",")
 # Basic Python to print hello world
 print("Hello World!")
 # print hello world from variable
