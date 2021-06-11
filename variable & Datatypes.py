1cc009eb5d4335864e6f5ee18843deedd86e1f26
Akshay Ambulkar <82163456+akshayambulkar16@users.noreply.github.com>
2021-06-11 16:09:05 +0530

    Add files via upload

---
+++ variable & Datatypes.py
@@ -0,0 +1,12 @@
+# what is variable:- A variable is name Given to a memory location in a program....
+# what is Datatype:- Primarily there are following datatype.....
+a = "Hello World" # a is string variable
+b = 232           # b is integer variable
+c = 21.34         # c is float
+d = True          # d is boolean
+print(a)
+# The type() function in Python returns the data type of the object passed to it as an argument. 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
